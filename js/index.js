1a206f20720ae8e08e5555259abcfe233e5ebfdf
Nazar <dunazar21@gmail.com>
2018-08-03 12:12:33 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function fib(n){
-if (n==1||n==2) return 1;
-else return fib(n-2)+fib(n-1);
-}
-const userNumber=+prompt('введіть номер числа');
-alert(fib(userNumber));
+if (n==1||n==2) {return 1;}
+else {return fib(n - 2) + fib(n - 1);
+}}
+const n=+prompt('введіть номер числа');
+alert(fib(n));
